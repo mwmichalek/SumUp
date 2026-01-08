9932f5d57d5dd58139cfd91ca6e809d7edf3234f
Michalek <mwmichalek@gmail.com>
2026-01-08 11:26:23 -0500

    Renames to SyncUp

--- src/SumUp.ClickUpService/V2/List/Item/TaskNamespace/TaskGetResponse_tasks_Helpers.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ClickUp.Api.V2.List.Item.TaskNamespace;
-
-public partial class TaskGetResponse_tasks
-{
-    public string? StatusName => Status.AsDictionary()?["status"]?.AsString();
-}
